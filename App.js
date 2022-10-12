fbe7aaf43d7efbd2a13396f3bbbf4061b4e6a344
wings1986 <wings1986@outlook.com>
2022-10-12 09:04:04 -0700

    fix issue with postmessage

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 
 const json = '{"display":"form","settings":{"pdf":{"id":"1ec0f8ee-6685-5d98-a847-26f67b67d6f0","src":"https://files.form.io/pdf/5692b91fd1028f01000407e3/file/1ec0f8ee-6685-5d98-a847-26f67b67d6f0"}},"components":[{"label":"Owner name","tableView":true,"key":"ownerName","type":"textfield","input":true},{"label":"Tenant name","tableView":true,"key":"tenantName","type":"textfield","input":true},{"label":"Description","autoExpand":false,"tableView":true,"key":"description","type":"textarea","input":true},{"label":"Upload photo","tableView":false,"image":true,"webcam":true,"fileTypes":[{"label":"","value":""}],"key":"uploadPhoto","type":"file","input":true},{"label":"Form letter details","attrs":[{"attr":"","value":""}],"refreshOnChange":false,"key":"formLetterDetails","type":"htmlelement","input":false,"tableView":false},{"type":"button","label":"Submit","key":"submit","disableOnInvalid":true,"input":true,"tableView":false}]}'
 
-export default async function App() {
+export default function App() {
   //for online formio
   // var customHTML = "<html><head> <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'><link rel='stylesheet' href='https://cdn.form.io/formiojs/formio.full.min.css'><script src='https://cdn.form.io/formiojs/formio.full.min.js'></script><script type='text/javascript'>window.onload = function() {Formio.createForm(document.getElementById('formio'), " + json + ");};</script></head><body><div id='formio'></div></body></html>"
 
@@ -45,8 +45,8 @@ export default async function App() {
         setBuiltInZoomControls={true}
         originWhitelist={['*']}
         //source={{ uri: (Platform.OS == "android" ? "file:///android_asset/www/4.html" : "4.html") }}
-        source={{ uri: "Web.bundle/4.html" }}
-        //source={sourceUri}
+        // source={{ uri: "Web.bundle/4.html" }}
+        source={ { uri: sourceUri }}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         onMessage={event => {
